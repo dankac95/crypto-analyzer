39089b20dfed237fecc237abc08dd3b9bf687b30
Daniel Kacprzak <daniel.kacprzak7@gmail.com>
2021-06-26 10:52:10 +0200

    Init project

---
+++ src/main/java/pl/daniel/cryptoanalyzer/CryptoAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package pl.daniel.cryptoanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/daniel/cryptoanalyzer/CryptoAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.daniel.cryptoanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
